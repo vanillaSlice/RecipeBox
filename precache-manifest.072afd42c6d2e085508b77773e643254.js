2dee989608a5443b7296efd182a0f5814bcfa2b3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-12 12:01:03 +0000

    Deploy recipebox.mikelowe.xyz to github.com/vanillaSlice/RecipeBox.git:gh-pages

---
+++ precache-manifest.072afd42c6d2e085508b77773e643254.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf6b3662e7c06f251ba8",
+    "url": "/static/css/main.97c9774c.chunk.css"
+  },
+  {
+    "revision": "bf6b3662e7c06f251ba8",
+    "url": "/static/js/main.bf6b3662.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a64fa4f11e514e2420aa",
+    "url": "/static/css/2.055df58e.chunk.css"
+  },
+  {
+    "revision": "a64fa4f11e514e2420aa",
+    "url": "/static/js/2.a64fa4f1.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "98609ea7507cc1c6a9be1087bde55bc4",
+    "url": "/index.html"
+  }
+];
